f1b3b61add9eee4e8e4ed44cfb960e073b747983
149521firemen <dimabelenov@gmail.com>
2018-11-04 13:27:24 +0200

    adds httpStatus package

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,13 @@
 import express = require('express');
-const router = express.Router();
-
 import { Request, Response } from 'express';
 import * as userService from './user.service';
+import * as httpStatus from 'http-status';
+
+const router = express.Router();
 
 router.route('/').get(async (req: Request, res: Response) => {
   const users = await userService.getUser();
-  res.send(users);
+  res.status(httpStatus.OK).json(users);
 });
 
 export default router;
