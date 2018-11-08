cee090a87e2830b0b5a9ac803571a2d0961d57cc
149521firemen <dimabelenov@gmail.com>
2018-11-08 16:45:29 +0200

    get all authors

--- src/index.ts
+++ src/index.ts
@@ -6,27 +6,35 @@ import { Author } from './entity/Author';
 
 createConnection()
   .then(async connection => {
-    let photo = new Photo();
-    photo.name = 'new photo';
-    photo.description = 'I am near polar bears';
-    photo.filename = 'photo-with-bears.jpg';
-    photo.isPublished = true;
-    photo.views = 10;
+    // let photo = new Photo();
+    // photo.name = 'new photo2';
+    // photo.description = 'I am near polar bears';
+    // photo.filename = 'photo-with-bears.jpg';
+    // photo.isPublished = true;
+    // photo.views = 10;
 
-    let author = new Author();
-    author.name = 'John Doe';
+    // let author = new Author();
+    // author.name = 'Smith';
 
-    photo.author = author;
+    // photo.author = author;
 
-    let photoRepository = connection.getRepository(Photo);
-    await photoRepository.save(photo);
+    // let photoRepository = connection.getRepository(Photo);
+    // await photoRepository.save(photo);
 
-    let photos = await connection
-      .getRepository(Photo)
-      .createQueryBuilder('photo')
-      .innerJoinAndSelect('photo.author', 'author')
+    // let photos = await connection
+    //   .getRepository(Photo)
+    //   .createQueryBuilder('photo')
+    //   .innerJoinAndSelect('photo.author', 'author')
+    //   .getMany();
+
+    // console.warn(photos);
+
+    let authorRepository = connection.getRepository(Author);
+    let authors = await connection
+      .getRepository(Author)
+      .createQueryBuilder('author')
       .getMany();
 
-    console.warn(photos);
+    console.warn(authors);
   })
   .catch(error => console.log(error));
