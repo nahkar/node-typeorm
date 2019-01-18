28c1831cccff4bbd8366fddd063906efbb6c946f
149521firemen <dimabelenov@gmail.com>
2019-01-18 14:45:50 +0200

    adds custom-env

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express = require('express');
 const app = express();
 import routes from './routes/v1';
 
-app.set('port', process.env.PORT || 8082);
+app.set('port', process.env.PORT);
 app.use('/v1', routes);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import * as dotenv from 'custom-env';
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import app from './app';
+
+dotenv.env(process.env.NODE_ENV);
 
 const init = async () => {
   try {
